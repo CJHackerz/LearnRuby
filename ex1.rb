c1eacf1865de7aa9448c04e19d315f3fe6690cc5
Chirag Jariwala <cjhackerz@yahoo.com>
2016-12-03 20:26:11 +0530

    ex1 done

--- ex1.rb
+++ ex1.rb
@@ -1,3 +1,7 @@
 puts "Hello World!"
-puts "This is fun!"
-puts "Lolz..."
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
